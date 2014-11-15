4b28dd26dc449f9eb72d27ca1de27b2fd3b42094
Matt Dodds <matthewrusselldodds@gmail.com>
2014-11-15 01:31:31 -0500

    Add imgur

--- lib/botylicious/imgur.rb
+++ lib/botylicious/imgur.rb
@@ -2,9 +2,12 @@ module Botylicious
   class Imgur
     include Cinch::Plugin
 
-    match /imgur tag (.+)/, { method: :fetch_tag }
+    match /imgur tag (.*)/, { method: :fetch_tag }
 
     def fetch_tag(m, tag)
+      tag = tag.split(' ').join('+')
+      html = Net::HTTP.get URI("http://imgur.com/search?q=#{tag}")
+      m.reply 'http:' + Nokogiri::HTML(html).xpath('//img').to_a.sample['src']
     end
   end
 end
